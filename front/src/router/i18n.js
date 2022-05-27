60cc9448d2d1dc2451ee5d52e3a779714529dfa2
obzect7 <oracle0405@gmail.com>
2022-05-27 20:55:33 +0900

    Changes

--- front/src/router/i18n.js
+++ front/src/router/i18n.js
@@ -18,8 +18,16 @@ module.exports = {
         name: '시스템 관리',
         users: {name: '사용자 관리'},
       },form: {
-        name: 'FORM 양식',
+        name: 'FORM 샘플',
         basic: {name: '기본 양식'},
+      },list: {
+        name: '리스트 샘플',
+      },details: {
+        name: '상세보기 샘플',
+      },result: {
+        name: '결과 페이지 샘플',
+      },components: {
+        name: '컴포넌트 샘플',
       },
     }
   }
